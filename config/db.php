8b76d5307d8ec0f03f90437ebb5050711a89b602
jrfx <eraldogoncalvesjr@gmail.com>
2017-04-02 14:40:59 -0300

    Finally fixed Composer

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  'development' => [
+      'driver'=>'mysql',
+      'host'=>'localhost',
+      'database'=>'adopet',
+      'username'=>'jrfx',
+      'password'=>'',
+      'charset'=>'utf8',
+      'collation'=>'utf8_unicode_ci'
+  ]
+];
